f52ac2cb8b7b732934e727491a105396c5f5c460
mengty <mengty@163.com>
2019-11-14 23:42:35 +0800

    dsd

--- src/main/java/com/git/HelloWorld.java
+++ src/main/java/com/git/HelloWorld.java
@@ -3,11 +3,7 @@ package com.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("b");
-		System.out.println("a");
+		System.out.println("gitWeb");
 
 		System.out.println("test-fz");
 	}
