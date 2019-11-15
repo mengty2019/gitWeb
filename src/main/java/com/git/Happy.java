2b38f79f3c15a4a31cf94ee4ba63a68001043776
mengty <mengty@163.com>
2019-11-15 18:06:20 +0800

    两个一块提交

--- src/main/java/com/git/Happy.java
+++ src/main/java/com/git/Happy.java
@@ -3,5 +3,8 @@ package com.git;
 public class Happy {
 
 	
-	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
 }
--- src/main/java/com/git/HelloWorld.java
+++ src/main/java/com/git/HelloWorld.java
@@ -3,7 +3,7 @@ package com.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("abc_gitWeb");
+		System.out.println("abc_gitWeb_00");
 
 		System.out.println("test-fz");
 	}
